1c24f68f77423e4beed8d686f76c991339b5fb41
furkeen <34249947+furkeen@users.noreply.github.com>
2020-02-13 14:10:51 +0300

    Add files via upload

---
+++ upload/catalog/controller/checkout/add.php
@@ -0,0 +1,171 @@
+<?php
+class ControllerCheckoutAdd extends Controller {
+
+    public function index(){
+        echo 'test';
+    }
+
+    public function toCart(){
+        
+        $product_id =  $this->request->get['product_id'];
+
+        $addtoCart = $this->add($product_id);
+
+        $result = json_decode($addtoCart , TRUE);
+
+        // print_r($result);
+        if(isset($result['redirect'])):
+            //The url you wish to send the POST request to
+            $url = $result['redirect'];
+            //The data you want to send via POST
+            $fields = [
+                'error_addtocart'      => $result,
+                'btnSubmit'         => 'Submit'
+            ];
+            //url-ify the data for the POST
+            $fields_string = http_build_query($fields);
+
+            //open connection
+            $ch = curl_init();
+
+            //set the url, number of POST vars, POST data
+            curl_setopt($ch,CURLOPT_URL, $url);
+            curl_setopt($ch,CURLOPT_POST, true);
+            curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+            //So that curl_exec returns the contents of the cURL; rather than echoing it
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+
+            //execute post
+            $result = curl_exec($ch);
+            echo $result;
+            // header('Location:'.$result['redirect']);
+        elseif(isset($result['success'])):
+            header('Location:http://localhost/oc3-4/index.php?route=checkout/cart');
+
+        endif;
+    }
+
+    
+	private function add($product_id) {
+		$this->load->language('checkout/cart');
+
+		$json = array();
+
+		// if (isset($this->request->post['product_id'])) {
+		// 	$product_id = (int)$this->request->post['product_id'];
+		// } else {
+		// 	$product_id = 0;
+		// }
+
+		$this->load->model('catalog/product');
+
+		$product_info = $this->model_catalog_product->getProduct($product_id);
+
+		if ($product_info) {
+			if (isset($this->request->post['quantity'])) {
+				$quantity = (int)$this->request->post['quantity'];
+			} else {
+				$quantity = 1;
+			}
+
+			if (isset($this->request->post['option'])) {
+				$option = array_filter($this->request->post['option']);
+			} else {
+				$option = array();
+			}
+
+			$product_options = $this->model_catalog_product->getProductOptions($product_id);
+
+			foreach ($product_options as $product_option) {
+				if ($product_option['required'] && empty($option[$product_option['product_option_id']])) {
+					$json['error']['option'][$product_option['product_option_id']] = sprintf($this->language->get('error_required'), $product_option['name']);
+				}
+			}
+
+			if (isset($this->request->post['recurring_id'])) {
+				$recurring_id = $this->request->post['recurring_id'];
+			} else {
+				$recurring_id = 0;
+			}
+
+			$recurrings = $this->model_catalog_product->getProfiles($product_info['product_id']);
+
+			if ($recurrings) {
+				$recurring_ids = array();
+
+				foreach ($recurrings as $recurring) {
+					$recurring_ids[] = $recurring['recurring_id'];
+				}
+
+				if (!in_array($recurring_id, $recurring_ids)) {
+					$json['error']['recurring'] = $this->language->get('error_recurring_required');
+				}
+			}
+
+			if (!$json) {
+				$this->cart->add($product_id, $quantity, $option, $recurring_id);
+
+				$json['success'] = sprintf($this->language->get('text_success'), $this->url->link('product/product', 'product_id=' . $product_id), $product_info['name'], $this->url->link('checkout/cart'));
+
+				// Unset all shipping and payment methods
+				unset($this->session->data['shipping_method']);
+				unset($this->session->data['shipping_methods']);
+				unset($this->session->data['payment_method']);
+				unset($this->session->data['payment_methods']);
+
+				// Totals
+				$this->load->model('setting/extension');
+
+				$totals = array();
+				$taxes = $this->cart->getTaxes();
+				$total = 0;
+		
+				// Because __call can not keep var references so we put them into an array. 			
+				$total_data = array(
+					'totals' => &$totals,
+					'taxes'  => &$taxes,
+					'total'  => &$total
+				);
+
+				// Display prices
+				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$sort_order = array();
+
+					$results = $this->model_setting_extension->getExtensions('total');
+
+					foreach ($results as $key => $value) {
+						$sort_order[$key] = $this->config->get('total_' . $value['code'] . '_sort_order');
+					}
+
+					array_multisort($sort_order, SORT_ASC, $results);
+
+					foreach ($results as $result) {
+						if ($this->config->get('total_' . $result['code'] . '_status')) {
+							$this->load->model('extension/total/' . $result['code']);
+
+							// We have to put the totals in an array so that they pass by reference.
+							$this->{'model_extension_total_' . $result['code']}->getTotal($total_data);
+						}
+					}
+
+					$sort_order = array();
+
+					foreach ($totals as $key => $value) {
+						$sort_order[$key] = $value['sort_order'];
+					}
+
+					array_multisort($sort_order, SORT_ASC, $totals);
+				}
+
+				$json['total'] = sprintf($this->language->get('text_items'), $this->cart->countProducts() + (isset($this->session->data['vouchers']) ? count($this->session->data['vouchers']) : 0), $this->currency->format($total, $this->session->data['currency']));
+			} else {
+				$json['redirect'] = str_replace('&amp;', '&', $this->url->link('product/product', 'product_id=' . $product_id));
+			}
+		}
+
+		return(json_encode($json));
+	}
+
+
+}
